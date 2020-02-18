067c3626fc0aeab0a069e62e6f6330e0b3fb9884
Kevin Mancilla <kmancilla1@ucol.mx>
2020-02-17 18:49:55 -0600

    doctor terminado

--- doctor.js
+++ doctor.js
@@ -1,9 +1,13 @@
 import nombre from "./nombre.js"
 export default class doctor{
-    constructor(){
-
+    constructor(nombre, especialidad, telefono, cedula){
+        this.nombre = nombre
+        this.especialidad = especialidad
+        this.telefono = telefono
+        this.cedula = cedula
     }
     getPerfil(){
-
+        return (`${this.cedula}, ${this.especialidad}, ${this.nombre.getNombreCompleto()}, ${this.telefono}`)
     }
 }
+
--- main.js
+++ main.js
@@ -7,5 +7,33 @@ import Paciente from "./paciente.js"
 import Tiempo from "./tiempo.js"
 
 class Main{
+    constructor(){
+        this.nombre1 = new Nombre("kevin", "rene", "mancilla")
+        this.doctor = new Doctor(this.nombre1,"jefe","3121105426","cedula:23245")
+        this.tiempo = new Tiempo("12", "32")
+    }
 
+    Nombre(){
+        
+        console.log(this.nombre1.getNombreCompleto())
+        console.log(this.nombre1.getApellidoNombre())
+        console.log(this.nombre1.getIniciales())
+
+    }
+
+    Doctor(){
+        console.log(this.doctor.getPerfil())
+    }
+
+    Tiempo(){
+        console.log(this.tiempo.getFormato12())
+        console.log(this.tiempo.getFormato24())
+    }
 }
+
+    let verificar = new Main()
+
+    verificar.Nombre()
+    verificar.Doctor()
+    verificar.Tiempo()
+
